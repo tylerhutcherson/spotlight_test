5e59579dcc7937968286eadc56a640af90d52cd5
tylerhutcherson <tyler.hutcherson@metismachine.com>
2018-03-02 14:17:30 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from spotlight.cross_validation import random_train_test_split
+from spotlight.datasets.movielens import get_movielens_dataset
+from spotlight.evaluation import rmse_score
+from spotlight.factorization.explicit import ExplicitFactorizationModel
+print('making model for spotlight', flush=True)
+dataset = get_movielens_dataset(variant='100K')
+
+train, test = random_train_test_split(dataset)
+
+model = ExplicitFactorizationModel(n_iter=1)
+model.fit(train)
+
+rmse = rmse_score(model, test)
+print(rmse, flush=True)
