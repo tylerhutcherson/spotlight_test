5db7dd9c7af22600519578f1a170d603ee0f5ff0
tylerhutcherson <tyler.hutcherson@metismachine.com>
2018-03-02 14:33:09 -0500

    try conda prehook

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
+print("Installing spotlight", flush=True)
+os.system("conda install -y -c maciejkula spotlight")
+
+print("Success!", flush=True)
 from spotlight.cross_validation import random_train_test_split
 from spotlight.datasets.movielens import get_movielens_dataset
 from spotlight.evaluation import rmse_score
 from spotlight.factorization.explicit import ExplicitFactorizationModel
 print('making model for spotlight', flush=True)
+
 dataset = get_movielens_dataset(variant='100K')
 
 train, test = random_train_test_split(dataset)
